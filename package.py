7edd99de0d10b6c4ef3e6452df2aca888b582f92
Bola E.Nasr <bola.e.nasr@gmail.com>
2019-08-05 11:21:49 +0200

    update package.py to use our rack servers

--- package.py
+++ package.py
@@ -1,2 +1,21 @@
-def install():
+def install(rack):
+    app = j.servers.gedis_websocket.default.app
+    rack.websocket_server_add("websocket", 4444, app)
+    dns = j.servers.dns.get_gevent_server("main", port=5354)  # for now high port
+    rack.add("dns", dns)
+    _gedis_server = j.servers.gedis.get("main", port=8900)
+    rack.add("gedis", _gedis_server.gedis_server)
+    rack.bottle_server_add()
+    print("hello")
+
+
+def uninstall():
+    print("hello")
+
+
+def prepare():
+    print("hello")
+
+
+def update():
     print("hello")
